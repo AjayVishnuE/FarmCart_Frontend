c748fed6a6a2ed65cf196745701e323c6b1e08c4
Paveena <pavnaptr@gmail.com>
2024-04-29 22:05:59 +0530

    famer changes

--- src/containers/FarmerChatPage/FarmerChatPage.jsx
+++ src/containers/FarmerChatPage/FarmerChatPage.jsx
@@ -11,6 +11,10 @@ function ChatPage() {
   const [query, setQuery] = useState('');
   const [responses, setResponses] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [isComplaintNext, setIsComplaintNext] = useState(false);
+  const accessToken = localStorage.getItem('accessToken');
+  const COMPLAINT_KEYWORDS = ["i want to register a complaint", "i have a complaint", "complaint about a product", "complaint regarding service", "file a complaint", "report an issue"];
+
 
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
@@ -20,11 +24,32 @@ function ChatPage() {
     e.preventDefault();
     if (!query.trim()) return;
     setLoading(true);
-
-    try {
-      const response = await axios.post(`${API_ENDPOINTS.chat}/chat/`, { query });
-      setResponses([...responses, { query, response: response.data.response }]);
+  
+    const isComplaintTrigger = COMPLAINT_KEYWORDS.some(keyword => query.toLowerCase().includes(keyword));
+  
+    if (isComplaintTrigger) {
+      setResponses([...responses, { query, response: 'Your next message will be registered as a complaint, and our technical team will look into it shortly.' }]);
+      setIsComplaintNext(true);
+      setLoading(false);
       setQuery('');
+      return; 
+    }
+  
+    const endpoint = isComplaintNext ? `${API_ENDPOINTS.chat}/complaint/` : `${API_ENDPOINTS.chat}/chat/`;
+    const requestBody = isComplaintNext ? { complaint: query } : { message: query };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    };
+  
+    try {
+      const response = await axios.post(endpoint, requestBody, config);
+      const responseMessage = isComplaintNext ? 'Your message has been registered as a complaint, and our technical team will look into it shortly.' : response.data.response;
+      setResponses([...responses, { query, response: responseMessage }]);
+      setQuery('');
+      setIsComplaintNext(false); 
     } catch (error) {
       console.error('Error:', error);
       setResponses([...responses, { query, response: 'Failed to get a response. Please try again.' }]);
@@ -75,7 +100,44 @@ function ChatPage() {
         </div>
       </form>
       
-      <FarmerNavbar/>
+      <div className='fnavbar-container' >
+          <Link to='/farmerdashboard'>
+        <svg c  id="farmdash-svg" className="mySvg" width="27" height="27" viewBox="0 0 27 27" fill="none" stroke='none' xmlns="http://www.w3.org/2000/svg">
+      <path d="M14.5958 1.55415C14.3052 1.26357 13.911 1.10033 13.5 1.10033C13.089 1.10033 12.6948 1.26357 12.4041 1.55415L1.55414 12.4042C1.27179 12.6965 1.11556 13.088 1.11909 13.4944C1.12263 13.9008 1.28564 14.2896 1.57302 14.577C1.8604 14.8644 2.24916 15.0274 2.65557 15.0309C3.06197 15.0344 3.45351 14.8782 3.74584 14.5959L4.19999 14.1417V24.35C4.19999 24.7611 4.36329 25.1553 4.65397 25.446C4.94466 25.7367 5.3389 25.9 5.74999 25.9H8.84999C9.26108 25.9 9.65532 25.7367 9.946 25.446C10.2367 25.1553 10.4 24.7611 10.4 24.35V21.25C10.4 20.8389 10.5633 20.4447 10.854 20.154C11.1447 19.8633 11.5389 19.7 11.95 19.7H15.05C15.4611 19.7 15.8553 19.8633 16.146 20.154C16.4367 20.4447 16.6 20.8389 16.6 21.25V24.35C16.6 24.7611 16.7633 25.1553 17.054 25.446C17.3447 25.7367 17.7389 25.9 18.15 25.9H21.25C21.6611 25.9 22.0553 25.7367 22.346 25.446C22.6367 25.1553 22.8 24.7611 22.8 24.35V14.1417L23.2541 14.5959C23.5465 14.8782 23.938 15.0344 24.3444 15.0309C24.7508 15.0274 25.1396 14.8644 25.427 14.577C25.7143 14.2896 25.8774 13.9008 25.8809 13.4944C25.8844 13.088 25.7282 12.6965 25.4458 12.4042L14.5958 1.55415V1.55415Z" stroke="#888888" stroke-width="2"/>
+      </svg>
+      </Link>
+      <Link to='/addproduct'>
+      <svg id="products-svg" className="mySvg" xmlns="http://www.w3.org/2000/svg" width="21" height="19" viewBox="0 0 21 19" fill="#888888">
+        <path d="M2.57141 5.50001H11.0214H10.5714H10.9214H2.57141ZM2.97141 3.50001H16.1714L15.3214 2.50001H3.82141L2.97141 3.50001ZM7.57141 10.25L9.57141 9.25001L11.5714 10.25V5.50001H7.57141V10.25ZM12.1214 18.5H2.57141C2.02141 18.5 1.55074 18.3043 1.15941 17.913C0.768078 17.5217 0.572078 17.0507 0.571411 16.5V4.02501C0.571411 3.79167 0.609078 3.56667 0.684411 3.35001C0.759744 3.13334 0.872078 2.93334 1.02141 2.75001L2.27141 1.22501C2.45474 0.991672 2.68374 0.812339 2.95841 0.687005C3.23308 0.561672 3.52074 0.499339 3.82141 0.500005H15.3214C15.6214 0.500005 15.9091 0.562672 16.1844 0.688005C16.4597 0.813339 16.6887 0.992339 16.8714 1.22501L18.1214 2.75001C18.2714 2.93334 18.3841 3.13334 18.4594 3.35001C18.5347 3.56667 18.5721 3.79167 18.5714 4.02501V8.92501C18.2547 8.80834 17.9297 8.71667 17.5964 8.65001C17.2631 8.58334 16.9214 8.55001 16.5714 8.55001V5.50001H13.5714V9.32501C12.9881 9.65834 12.4797 10.071 12.0464 10.563C11.6131 11.055 11.2714 11.609 11.0214 12.225L9.57141 11.5L5.57141 13.5V5.50001H2.57141V16.5H10.9214C11.0547 16.8833 11.2214 17.2417 11.4214 17.575C11.6214 17.9083 11.8547 18.2167 12.1214 18.5ZM15.5714 18.5V15.5H12.5714V13.5H15.5714V10.5H17.5714V13.5H20.5714V15.5H17.5714V18.5H15.5714Z" fill="#888888"/>
+        </svg>
+        </Link>
+        <Link to='/farmerchat'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="61" height="60" viewBox="0 0 61 50" fill="none">
+        <g filter="url(#filter0_d_775_3908)">
+        <path d="M20.8572 34V16C20.8572 15.45 21.0532 14.9793 21.4452 14.588C21.8372 14.1967 22.3078 14.0007 22.8572 14H38.8572C39.4072 14 39.8782 14.196 40.2702 14.588C40.6622 14.98 40.8578 15.4507 40.8572 16V28C40.8572 28.55 40.6615 29.021 40.2702 29.413C39.8788 29.805 39.4078 30.0007 38.8572 30H24.8572L20.8572 34ZM24.8572 26H32.8572V24H24.8572V26ZM24.8572 23H36.8572V21H24.8572V23ZM24.8572 20H36.8572V18H24.8572V20Z" fill="#7519EB"/>
+        </g>
+        <defs>
+        <filter id="filter0_d_775_3908" x="-1" y="-2.5" width="64" height="64" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="6"/>
+        <feGaussianBlur stdDeviation="10"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0.458824 0 0 0 0 0.0980392 0 0 0 0 0.921569 0 0 0 0.4 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_775_3908"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_775_3908" result="shape"/>
+        </filter>
+        </defs>
+        </svg></Link>
+         <Link to='/farmerprofile'>
+        <svg id="profile-svg" className="mySvg" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="user">
+          <path id="Vector" d="M20.1429 21.5V19.5C20.1429 18.4391 19.7214 17.4217 18.9713 16.6716C18.2211 15.9214 17.2037 15.5 16.1429 15.5H8.14285C7.08199 15.5 6.06457 15.9214 5.31443 16.6716C4.56428 17.4217 4.14285 18.4391 4.14285 19.5V21.5" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path id="Vector_2" d="M12.1429 11.5C14.352 11.5 16.1429 9.70914 16.1429 7.5C16.1429 5.29086 14.352 3.5 12.1429 3.5C9.93371 3.5 8.14285 5.29086 8.14285 7.5C8.14285 9.70914 9.93371 11.5 12.1429 11.5Z" stroke="#888888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </g>
+      </svg></Link>
+            
+        </div>
+      
     </div>
   );
 }
